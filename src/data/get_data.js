0d1aaf6d5edd95316eedc290cc5faeb1efd518d1
Andrey Sery <andrey.72006@gmail.com>
2024-01-29 23:56:32 +0300

    get list users and render

--- src/data/get_data.js
+++ src/data/get_data.js
@@ -1,19 +1,27 @@
 export async function getDataFromPlaceholder(parentItem) {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
 
     const listElement = document.createElement("ul");
 
     data.forEach((user) => {
       const userElement = document.createElement("li");
-      userElement.textContent = user.title;
+      userElement.style.display = "flex";
+      userElement.style.flexDirection = "column";
+      userElement.style.gap = "15px";
+      userElement.style.marginBottom = "20px";
 
-      const coverElement = document.createElement("img");
-      coverElement.alt = "cover";
-      coverElement.src = user.url;
+      const userNameElement = document.createElement("h3");
+      userNameElement.textContent = user.name;
 
-      userElement.append(coverElement);
+      const userEmailElement = document.createElement("span");
+      userEmailElement.textContent = user.email;
+
+      const userPhoneElement = document.createElement("span");
+      userPhoneElement.textContent = user.phone;
+
+      userElement.append(userNameElement, userEmailElement, userPhoneElement);
       listElement.append(userElement);
     });
 
